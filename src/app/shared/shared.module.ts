d2adad13c8a4335cb9f2305c0008c1a3c6681532
Oscar Gaytan <oscar.gaytan@cloudsourceit.com>
2021-06-21 07:36:25 -0500

    Cleaned up sidebar and header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent
   ],
   exports: [
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
